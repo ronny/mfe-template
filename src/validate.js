98895b556ade756c9355f6a597e55b1efcb48634
Lucas Parry <lparry@gmail.com>
2015-11-25 16:34:39 +1100

    split attribute validators off into another file

--- src/validate.js
+++ src/validate.js
@@ -1,3 +1,5 @@
+import { booleanValueValidator, selectValueValidator } from "./attribute-validators"
+
 function validate(itemSubmission, rules, itemTypes) {
   const itemType = itemTypes.find(type => type.uuid == itemSubmission.itemTypeUuid);
   let itemSubmissionErrors = {};
@@ -9,29 +11,6 @@ function validate(itemSubmission, rules, itemTypes) {
     itemSubmissionErrors,
     attributeErrors
   }};
-}
-
-let booleanValueValidator = (value, label) => {
-  let result =  { valid: true }
-
-  if (value === "yes" || value === "no") {
-    result.valid = false;
-    result.message = label + " does not have a valid value selected"
-  }
-
-  return result
-}
-
-let selectValueValidator = (value, label, fieldOptions) => {
-  let result =  { valid: true }
-  let matchedValue = fieldOptions.options.find(opt => opt === value)
-
-  if (!matchedValue) {
-    result.valid = false;
-    result.message = label + " does not have a valid value selected"
-  }
-
-  return result
 }
 
 function validateAttributesWithRestrictedValues(attributeValues, itemType) {
@@ -64,26 +43,6 @@ function validateAttributesWithRestrictedValues(attributeValues, itemType) {
   }
 
   return errors;
-// item_submission_attributes[:attributeValues].each do |attribute_id, attribute_values|
-//   definition = item_type.attribute_definitions[attribute_id]
-//   definition.options.each_with_index do |options, position|
-//     validator_class = case options.fetch("type")
-//                       when "select"
-//                         AttributeSelectOptionValid
-//                       when "boolean"
-//                         AttributeIsBoolean
-//                       else
-//                         next
-//                       end
-//     validator = validator_class.new(attribute_values, "#{definition.label}:#{position}", definition)
-//     next if validator.valid?
-//
-//     attribute_values_errors[attribute_id] ||= []
-//     attribute_values_errors[attribute_id] << validator.message
-//
-//
-//   end
-// end
 }
 
 
